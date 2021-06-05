82a2dcaa9e8813376b91385ea79fdb0e2519af8f
Shouraa <shorajafar@gmail.com>
2021-06-05 20:14:53 +0300

    deploy

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const cors = require('cors');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const logoutRouter = require('./controllers/logout');
-const protectedRouter = require('./controllers/protected');
-const loginRouter = require('./controllers/login');
-const usersRouter = require('./controllers/users');
+// const logoutRouter = require('./controllers/logout');
+// const protectedRouter = require('./controllers/protected');
+// const loginRouter = require('./controllers/login');
+// const usersRouter = require('./controllers/users');
+// const refreshtokenRouter = require('./controllers/refresh_token');
+// const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
-const refreshtokenRouter = require('./controllers/refresh_token');
-const middleware = require('./utils/middleware');
 
 const url = config.MONGODB_URI;
 
@@ -29,17 +29,19 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(middleware.tokenExtractor);
 
 app.use('/api/blogs', blogsRouter);
+
 app.get('/', (req, res) => {
   res.send('Welcome to the app API');
 });
-app.use('/api/users', usersRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/protected', protectedRouter);
-app.use('/api/refresh_token', refreshtokenRouter);
-app.use('/api/logout', logoutRouter);
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+// app.use('/api/users', usersRouter);
+// app.use('/api/login', loginRouter);
+// app.use('/api/protected', protectedRouter);
+// app.use('/api/refresh_token', refreshtokenRouter);
+// app.use('/api/logout', logoutRouter);
+
+// app.use(middleware.unknownEndpoint);
+// app.use(middleware.errorHandler);
 
 // Database Connection
 mongoose
