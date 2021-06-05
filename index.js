200047167ac2155bf8cc82bc8f236c0b5972e923
Shouraa <shorajafar@gmail.com>
2021-06-05 20:48:25 +0300

    deploy

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
-const http = require('http');
-const app = require('./app');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const server = http.createServer(app);
+// const logoutRouter = require('./controllers/logout');
+// const protectedRouter = require('./controllers/protected');
+// const loginRouter = require('./controllers/login');
+// const usersRouter = require('./controllers/users');
+// const refreshtokenRouter = require('./controllers/refresh_token');
+// const middleware = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+logger.info('connecting to', config.MONGODB_URI);
+
+// app.use(express.static('build'));
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(middleware.tokenExtractor);
+
+app.use('/api/blogs', blogsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the app API');
 });
+
+// app.use('/api/users', usersRouter);
+// app.use('/api/login', loginRouter);
+// app.use('/api/protected', protectedRouter);
+// app.use('/api/refresh_token', refreshtokenRouter);
+// app.use('/api/logout', logoutRouter);
+
+// app.use(middleware.unknownEndpoint);
+// app.use(middleware.errorHandler);
+
+const url = config.MONGODB_URI;
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() =>
+    app.listen(config.PORT, () =>
+      logger.info(`Server Running on Port: http://localhost:${config.PORT}`)
+    )
+  )
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+// {
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   }
